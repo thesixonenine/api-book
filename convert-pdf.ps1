a4963ec4addb3e10c1b6441b34d3675e63aae2fc
Simple <huyang1994822@gmail.com>
2023-08-18 18:17:21 +0800

    update

--- convert-pdf.ps1
+++ convert-pdf.ps1
@@ -1,2 +1,3 @@
 Remove-Item -Force -ErrorAction SilentlyContinue main.pdf
 asciidoctor-pdf --theme resources/themes/theme.yml -a pdf-fontsdir=resources/fonts main.adoc
+# asciidoctor-pdf -a pdf-fontsdir=C:\Windows\Fonts -a pdf-fonts=微软雅黑 main.adoc
